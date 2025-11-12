275bf895d815efff7d4dfe936e7d21ddc161aac0
Marilyn <73735993+marilyn-croffie@users.noreply.github.com>
2025-11-12 16:56:03 -0600

    Update geometry logic to be slope-independent

--- src/GeometryUtils.java
+++ src/GeometryUtils.java
@@ -4,29 +4,50 @@ public final class GeometryUtils
   
   private GeometryUtils(){} // private constructor to prevent instantiation
 
+  public static Point toVector(Point p1, Point p2)
+  {
+    double dx = p2.getX() - p1.getX();
+    double dy = p2.getY() - p1.getY();
+
+    return Point.of(dx, dy);
+  }
+
+  private static boolean isZero(double value)
+  {
+    return Math.abs(value) < EPSILON;
+  }
+
+  public static boolean areParallel(Point v1, Point v2)
+  {
+    double cross = v1.getY() * v2.getX() - v2.getY() * v1.getX();
+    
+    return isZero(cross);
+  }
+
+  public static arePerpendicular(Point v1, Point v2)
+  {
+    double dot = v1.getX() * v2.getX() + v1.getY() * v2.getY();
+
+    return isZero(dot);
+  }
+    
   // Check if three points are collinear
   public static boolean areCollinear(Point p1, Point p2, Point p3)
   {
-    // Using slope formula: (y2 - y1)/(x2 - x1) == (y3 - y1)/(x3 - x1)
-    // Avoid divide by zero by cross-multiplying
-    double dx1 = p2.getX() - p1.getX();
-    double dy1 = p2.getY() - p1.getY();
-    double dx2 = p3.getX() - p1.getX();
-    double dy2 = p3.getY() - p1.getY();
+    Point v1 = toVector(p1, p2);
+    Point v2 = toVector(p1, p3);
 
-    return ((dy1 * dx2 - dy2 * dx1) < EPSILON);
+    return areParallel(v1, v2);
   }
 
   // Check if four points are collinear (any three forming a line)
-  public static boolean areAnyThreeCollinear(Point p1, Point p2, Point p3, Point p4) 
+  public static boolean areCollinear(Point p1, Point p2, Point p3, Point p4) 
   {
         return areCollinear(p1, p2, p3) ||
                areCollinear(p1, p2, p4) ||
-               areCollinear(p1, p3, p4) ||
-               areCollinear(p2, p3, p4);
+               areCollinear(p1, p3, p4);
   }
-
-  
 }
+
 
 

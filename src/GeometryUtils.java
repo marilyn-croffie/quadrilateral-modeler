c56bff2ae28ea3d09a654d2ba449fd1d1036b185
Marilyn <73735993+marilyn-croffie@users.noreply.github.com>
2025-11-13 03:09:42 -0600

    Update GeometryUtils.java

--- src/GeometryUtils.java
+++ src/GeometryUtils.java
@@ -54,6 +54,18 @@ public final class GeometryUtils
 
     return Math.sqrt(v.getX() * v.getX() + v.getY() * v.getY());
   }
+  
+  public static double height(Point p1, Point p2, Point p3)
+  {
+    Point lineVector =  toVector(p1,p2);
+    Point pointVector =  toVector(p1,p3);
+    
+    // Cross product magnitude divided by line length
+    double cross = Math.abs(lineVector.getX() * pointVector.getY() - lineVector.getY() * pointVector.getX());
+    double lineLength = distance(p1, p2);
+    
+    return cross / lineLength;
+  }
 
   private Point[] sortPointsCyclic(Point[] points) 
   {
@@ -78,13 +90,52 @@ public final class GeometryUtils
     });
 
     return points;
-} 
+  } 
 
-  public static double height(Point p1, Point p2)
+  private boolean isParallelOrder(Point[] points)
   {
+    Point sideA = toVector(points[0],points[1]);
+    Point sideB = toVector(points[2],points[3]);
+    Point sideC = toVector(points[1],points[2]);
+    Point sideD = toVector(points[3],points[0]);
 
+    boolean pair1 = areParallel(sideA, sideB);
+    boolean pair2 = areParallel(sideC, sideD);
+
+    List<Point> pts = new ArrayList<>();
+
+    if (pair1)
+    {
+      return true;
+    }
+    else if (pair2)
+    {
+      return false;
+    }
+    else
+    {
+      // No parallel sides
+      throw new IllegalArgumentException("Points do not form a quadrilateral with parallel sides");
+    }
   }
+
+  public static Points[] reOrder(Point p1, Point p2, Point p3, Point p4)
+  {
+    Point[] pts = new Point[]{p1, p2, p3, p4};
+    pts = sortPointCyclic(pts);
+    
+    if (isParallelOrder(pts))
+    {
+      return pts;
+    }
+    else
+    {
+      pts = new Point[]{p2, p3, p4, p1};
+      return pts;
+    }
+  } 
 }
+
 
 
 
